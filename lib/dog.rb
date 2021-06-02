5b37aa6c73de5768935ef48db4444a1284ab2920
DJ Taylor <dtaylor88@gmail.com>
2021-06-02 22:32:22 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,12 @@
 class Dog 
   ##writer method / setter 
   def name=(dog_name) 
-    this_dogs_name = dog_name 
+    @this_dogs_name = dog_name 
   end
   
   ##reader method  / getter 
   def name
-    this_dogs_name
+    @this_dogs_name
   end
 end
 
